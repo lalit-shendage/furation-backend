b009a5790a4301babe24157520e15d4dbac2f8f1
lalit-shendage <shendage.lalit@gmail.com>
2023-06-19 15:58:15 +0530

    setting up validation middleware

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,1 +1,21 @@
-const verifyToken=null;
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET="Ghost"
+const verifyToken = async (req, res, next) => {
+  const token = req.header("auth-token");
+// checking token is provided or not 
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+//   decoding the token
+  try {
+    const decoded = await jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = { verifyToken };
