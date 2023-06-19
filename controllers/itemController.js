f77d2441d5d89c513a2c068efdf7ca0baae75c25
lalit-shendage <shendage.lalit@gmail.com>
2023-06-19 16:35:42 +0530

    fetchAllItems, fetchItemById added

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -12,12 +12,17 @@ const fetchAllItems =async(req,res)=>{
 }
 const fetchItemById =async(req,res)=>{
     try {
-         // fetching all items from database 
-    const items=await Item.find();
-
-    res.json({items})
+        // fetching id bu req
+    const { id } = req.params;
+    
+    const item = await Item.findById(id);
+    // if item of give id is not present
+    if (!item) {
+        return res.status(404).json({ message: 'Item not found' });
+      }
+      res.json({ item });
     } catch (error) {
-        res.status(500).json({ message: 'Failed to fetch items' });
+        res.status(500).json({ message: 'Failed to fetch item' });
     }
 }
 
