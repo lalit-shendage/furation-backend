9eb1dfc8a153d0771621647766fbef828787e121
lalit-shendage <shendage.lalit@gmail.com>
2023-06-19 15:35:57 +0530

    folder structure creation

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const database = require('./config/database');
+const userRoutes= require('./routes/userRoutes');
+const itemRoutes=require('./routes/itemRoutes')
 
 database();
 
+// parse incoming request bodies
 app.use(bodyParser.json());
+
+app.use(`/api`,userRoutes);
+app.use(`/api/items`,itemRoutes);
 
 app.listen(5000, () => {
   console.log('Server started on port 5000');
